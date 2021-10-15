9bbf965052e2067390b4dcbb60770a2fb8bc20a6
Dan <olveradan@hotmail.com>
2021-10-15 15:11:41 -0500

    Cliente que recibe las noticias de un grupo multicast
    Ventana y funcionalidad

--- NoticiasHilos_Cliente/MainWindow.xaml.cs
+++ NoticiasHilos_Cliente/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +23,60 @@ namespace NoticiasHilos_Cliente
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread thread;
+        private UdpClient udpClient;
+        private string IP_ADDR = "224.2.2.3";
+        private int PORT = 8888;
+        private string noticia;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Click_btn_iniciar(object sender, RoutedEventArgs e)
+        {
+            udpClient = new UdpClient(PORT);
+            udpClient.JoinMulticastGroup(IPAddress.Parse(IP_ADDR));
+
+            thread = new Thread(new ThreadStart(EscucharNoticias));
+            thread.Start();
+        }   
+
+        private void EscucharNoticias()
+        {
+            try
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    tb_noticias.AppendText("Bienvenido al noticiero");
+                });
+                
+                while (true)
+                {
+                    var ipEndPoint = new IPEndPoint(IPAddress.Any, 0);
+                    var data = udpClient.Receive(ref ipEndPoint);
+
+                    Console.WriteLine(Encoding.Default.GetString(data));
+
+                    noticia = Encoding.Default.GetString(data);
+                    noticia = String.Format("\nNoticia: {0}\n", noticia);
+
+                    this.Dispatcher.Invoke(() =>
+                    {
+                        tb_noticias.AppendText(noticia);
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                tb_noticias.Clear();
+                Console.WriteLine(ex.StackTrace);
+            }
+        }
+
+        private void Click_btn_salir(object sender, RoutedEventArgs e)
+        {
+            thread.Abort();
         }
     }
 }
